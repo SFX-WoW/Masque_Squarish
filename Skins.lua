f8439b80710df81c50624db8618566725955954a
StormFX <stormfx@gmail.com>
2023-05-01 13:04:49 -0500

    - Use a packager token for `Version`.

--- Skins.lua
+++ Skins.lua
@@ -32,7 +32,7 @@ local L = Core.Locale
 local API_VERSION = 100100
 
 -- Skin Info
-local Version = GetAddOnMetadata(AddOn, "Version")
+local Version = "@project-version@"
 local Websites = {
 	"https://github.com/SFX-WoW/Masque_Squarish",
 	"https://www.curseforge.com/wow/addons/masque-squarish",
